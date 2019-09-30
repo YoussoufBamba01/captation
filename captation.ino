79ade1d13bfe83e406d50959efb2dd684242afbd
Youssoufba <youssouf.bamba@investivgroup.com>
2019-09-30 16:42:44 +0200

    client ESP

--- captation.ino
+++ captation.ino
@@ -1,49 +1,80 @@
-#include <ESP8266HTTPClient.h>
-#include <ESP8266WiFi.h>
+#include <Arduino.h>
 
-void setup() 
-{
+#include <ESP8266WiFi.h>
+#include <ESP8266WiFiMulti.h>
+
+#include <ESP8266HTTPClient.h>
+
+#include <WiFiClient.h>
+
+ESP8266WiFiMulti WiFiMulti;
+
+float hum = 10.0;
+float tem = 24.2;
+
+void setup() {
+
+  Serial.begin(115200);
   
-  Serial.begin(115200);               
-  WiFi.begin("yourSSID", "yourPASS");   
-  while (WiFi.status() != WL_CONNECTED)
-     {  //Wait for the WiFI connection completion
-   
-      delay(500);
-      Serial.println("Waiting for connection");
-   
-      }
+
+  Serial.println();
+  Serial.println();
+  Serial.println();
+
+  for (uint8_t t = 4; t > 0; t--) {
+    Serial.printf("[SETUP] WAIT %d...\n", t);
+    Serial.flush();
+    delay(1000);
+  }
+
+  WiFi.mode(WIFI_STA);
+  WiFiMulti.addAP("SMART_GRID", "password");
+  WiFiMulti.addAP("AFRIQUE PHYTO PLUS", "password");
+  
 
 }
 
-void loop() 
-{
-    
-    if(WiFi.status()== WL_CONNECTED)
-    
-        {  
-       
-         HTTPClient http;   
-       
-         http.begin("http://localhost8000/show");      
-         http.addHeader("Content-Type", "text/plain");  
-       
-         int httpCode = http.POST("Message from ESP8266");   
-         String payload = http.getString();                 
-         Serial.println(httpCode);   
-         Serial.println(payload);    
-       
-         http.end();
-       
-         }
-      else
-         {
-       
-          Serial.println("Error in WiFi connection");   
-       
-          }
- 
-  delay(30000); 
- 
+void loop() {
+  // wait for WiFi connection
+  if ((WiFiMulti.run() == WL_CONNECTED)) {
 
+    WiFiClient client;
+
+    HTTPClient http;
+
+
+
+    Serial.print("[HTTP] begin...\n");
+    String url = "http://ef14b3a3.ngrok.io/post?data1=";
+    url += (String)hum;
+    url += "&data2=";
+    url += (String)tem;
+    Serial.println(url);
+    if (http.begin(client, url)) {  // HTTP
+
+      Serial.print("[HTTP] GET...\n");
+      // start connection and send HTTP header
+      int httpCode = http.GET();
+
+      
+      if (httpCode > 0) {
+        // HTTP header has been send and Server response header has been handled
+        Serial.printf("[HTTP] GET... code: %d\n", httpCode);
+
+        
+        if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY) {
+          String payload = http.getString();
+          Serial.println(payload);
+        }
+      } else {
+        Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
+      }
+
+      http.end();
+    } else {
+      Serial.printf("[HTTP} Unable to connect\n");
+    }
+  }
+
+  delay(10000);
 }
