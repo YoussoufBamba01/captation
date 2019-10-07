8478a7bd8660aea5501d51df125ddcc73c8627f0
Youssoufba <youssouf.bamba@investivgroup.com>
2019-10-07 17:33:19 +0200

    post vers un serveur

--- captation.ino
+++ captation.ino
@@ -1,80 +1,47 @@
-#include <Arduino.h>
-
-#include <ESP8266WiFi.h>
-#include <ESP8266WiFiMulti.h>
-
 #include <ESP8266HTTPClient.h>
-
-#include <WiFiClient.h>
-
-ESP8266WiFiMulti WiFiMulti;
-
-float hum = 10.0;
-float tem = 24.2;
-
-void setup() {
-
-  Serial.begin(115200);
-  
-
-  Serial.println();
-  Serial.println();
-  Serial.println();
-
-  for (uint8_t t = 4; t > 0; t--) {
-    Serial.printf("[SETUP] WAIT %d...\n", t);
-    Serial.flush();
-    delay(1000);
+#include <ESP8266WiFi.h>
+ 
+void setup() 
+{
+ 
+  Serial.begin(115200);                                 
+  WiFi.begin("SSID", "*****");   
+  while (WiFi.status() != WL_CONNECTED) 
+  {  
+ 
+    delay(500);
+    Serial.println("Waiting for connection");
+ 
   }
-
-  WiFi.mode(WIFI_STA);
-  WiFiMulti.addAP("SMART_GRID", "password");
-  WiFiMulti.addAP("AFRIQUE PHYTO PLUS", "password");
-  
-
+ 
 }
-
-void loop() {
-  // wait for WiFi connection
-  if ((WiFiMulti.run() == WL_CONNECTED)) {
-
-    WiFiClient client;
-
-    HTTPClient http;
-
-
-
-    Serial.print("[HTTP] begin...\n");
-    String url = "http://ef14b3a3.ngrok.io/post?data1=";
-    url += (String)hum;
-    url += "&data2=";
-    url += (String)tem;
-    Serial.println(url);
-    if (http.begin(client, url)) {  // HTTP
-
-      Serial.print("[HTTP] GET...\n");
-      // start connection and send HTTP header
-      int httpCode = http.GET();
-
+ 
+void loop() 
+{
+ 
+ if(WiFi.status()== WL_CONNECTED)
+ {   
+  
+   HTTPClient http;    
+ 
+   http.begin("http://bdd3583d.ngrok.io/post/?data1=400&data2=410");      
+   http.addHeader("Content-Type", "application/x-www-form-urlencoded"); 
+  
+   int httpCode = http.POST("data1=400&data2=410");  
+                 
+ 
+   Serial.println(httpCode);   
       
-      if (httpCode > 0) {
-        // HTTP header has been send and Server response header has been handled
-        Serial.printf("[HTTP] GET... code: %d\n", httpCode);
-
-        
-        if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY) {
-          String payload = http.getString();
-          Serial.println(payload);
-        }
-      } else {
-        Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
-      }
-
-      http.end();
-    } else {
-      Serial.printf("[HTTP} Unable to connect\n");
-    }
-  }
-
-  delay(10000);
+   http.end();  
+ 
+ }
+ else
+ {
+ 
+    Serial.println("Error in WiFi connection");   
+ 
+ }
+ 
+  delay(30000);  
+ 
 }
